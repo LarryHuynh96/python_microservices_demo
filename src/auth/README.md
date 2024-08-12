af6f589a43e91404d651b1028c7fddfeda9c86a4
Phuc Huynh-Van <phuc.huynh-van@knistr.com>
2024-08-12 17:15:25 +0700

    auth service: Add route DB add init Data

---
+++ src/auth/README.md
@@ -0,0 +1,7 @@
+# Auth Service
+
+## Description
+
+## How to run:
+    1. Install dependencies
+    2. Run the service
--- src/auth/server.py
+++ src/auth/server.py
@@ -12,9 +12,38 @@ server = Flask(__name__)
 mysql = MySQL(server)
 
 # MySQL configurations
-server.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-server.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-server.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-server.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-server.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT')
+server.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+server.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+server.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
 
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return jsonify({"message": "Missing username or password"}), 401
+
+    cur = mysql.connection.cursor()
+    res = cur.execute(f"SELECT * FROM users WHERE username = '{auth.username}'")
+
+    if res > 0:
+        user = cur.fetchone()
+        if user[1] != auth.password and user[0] != auth.username:
+            return jsonify({"message": "Invalid credentials"}), 401
+        else:
+            return jsonify(
+                {
+                    "token": jwt.encode(
+                        {
+                            "user": auth.username,
+                            "exp": datetime.datetime.utcnow()
+                            + datetime.timedelta(minutes=30),
+                        },
+                        os.getenv("JWT_SECRET_KEY"),
+                    )
+                }
+            )
+    else:
+        return jsonify({"message": "User not found"}), 404
