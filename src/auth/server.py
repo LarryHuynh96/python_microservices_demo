f6b47c3c428edb514fdb98ac83cf55746ff9492e
Phuc Huynh-Van <phuc.huynh-van@knistr.com>
2024-08-23 17:13:38 +0700

    auth service: Add route DB add init Data

--- src/auth/server.py
+++ src/auth/server.py
@@ -23,7 +23,7 @@ server.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
 def login():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
-        return jsonify({"message": "Missing username or password"}), 401
+        return "Missing username or password", 401
 
     cur = mysql.connection.cursor()
     res = cur.execute(f"SELECT * FROM users WHERE username = '{auth.username}'")
@@ -31,19 +31,35 @@ def login():
     if res > 0:
         user = cur.fetchone()
         if user[1] != auth.password and user[0] != auth.username:
-            return jsonify({"message": "Invalid credentials"}), 401
+            return "Invalid credentials", 401
         else:
-            return jsonify(
-                {
-                    "token": jwt.encode(
-                        {
-                            "user": auth.username,
-                            "exp": datetime.datetime.utcnow()
-                            + datetime.timedelta(minutes=30),
-                        },
-                        os.getenv("JWT_SECRET_KEY"),
-                    )
-                }
-            )
+            return create_jwt(auth.username, os.getenv("SECRET_KEY"), True)
     else:
-        return jsonify({"message": "User not found"}), 404
+        return "User not found", 404
+
+
+def create_jwt(username, secret_key, is_admin):
+    payload = {
+        "username": username,
+        "admin": is_admin,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+        "iat": datetime.datetime.utcnow(),
+    }
+    return jwt.encode(payload, secret_key, algorithm="HS256")
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_token = request.headers.get("Authorization")
+    if not encoded_token:
+        return jsonify({"message": "Missing token"}), 401
+
+    encoded_token = encoded_token.split(" ")[1]
+    try:
+        decoded_token = jwt.decode(encoded_token, os.getenv("SECRET_KEY"), algorithms=[
+            "HS256"])
+    except:
+        return "Not authorize", 401
+    return decoded_token, 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
